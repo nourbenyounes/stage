109a3ff5ed4f7bb13514a68ad919901edbdc7233
omarkdhai <129288147+omarkdhai@users.noreply.github.com>
2024-04-22 10:56:18 +0100

    Add files via upload

---
+++ src/main/java/enicarthage/Projetweb/entity/DatabaseSequence.java
@@ -0,0 +1,35 @@
+package Entities;
+
+import org.springframework.data.annotation.Id;
+
+public class DatabaseSequence {
+	
+	@Id
+    private String id;
+    private int seq;
+
+    public DatabaseSequence() {
+    }
+
+    public DatabaseSequence(String id, int seq) {
+        this.id = id;
+        this.seq = seq;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+}
