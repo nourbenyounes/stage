696be7d1e7df56187eb1fe261bd6d5d887bdd50e
omarkdhai <129288147+omarkdhai@users.noreply.github.com>
2024-04-22 22:40:31 +0100

    Add files via upload

---
+++ src/main/java/enicarthage/Projetweb/entity/Message.java
@@ -0,0 +1,48 @@
+package enicarthage.Projetweb.entity;
+
+
+import java.util.Date;
+
+import jakarta.persistence.Id;
+
+public class Message {
+	@Id
+	private String senderEmail;
+    private Date time = new Date(System.currentTimeMillis());
+    private String replymessage;
+
+    public Message() {
+    }
+
+    public Message(String senderEmail, Date time, String replymessage) {
+        this.senderEmail = senderEmail;
+        this.time = time;
+        this.replymessage = replymessage;
+    }
+
+    public String getSenderEmail() {
+        return senderEmail;
+    }
+
+    public void setSenderEmail(String senderEmail) {
+        this.senderEmail = senderEmail;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getReplymessage() {
+        return replymessage;
+    }
+
+    public void setReplymessage(String replymessage) {
+        this.replymessage = replymessage;
+    }
+
+
+}
