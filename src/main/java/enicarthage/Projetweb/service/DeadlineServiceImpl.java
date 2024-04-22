f56479389ef3b2f237dd5339bbbc441e4728984a
Sawssan123 <125601744+Sawssan123@users.noreply.github.com>
2024-04-23 00:56:03 +0100

    Update DeadlineServiceImpl.java

--- src/main/java/enicarthage/Projetweb/service/DeadlineServiceImpl.java
+++ src/main/java/enicarthage/Projetweb/service/DeadlineServiceImpl.java
@@ -4,6 +4,7 @@ package enicarthage.Projetweb.service;
 import enicarthage.Projetweb.entity.Deadline;
 import enicarthage.Projetweb.entity.Etudiant;
 import enicarthage.Projetweb.repository.DeadlineRepository;
+import enicarthage.Projetweb.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Date;
@@ -16,6 +17,9 @@ public class DeadlineServiceImpl implements DeadlineService {
 
     @Autowired
     private DeadlineRepository deadlineRepository;
+    @Autowired
+    private NotificationRepository notificationRepository;
+
 
     @Override
     public Deadline createDeadline(String description, Date date) {
@@ -25,11 +29,19 @@ public class DeadlineServiceImpl implements DeadlineService {
 
     @Override
     public Deadline ajoutDeadline(Deadline deadline) {
+        //yeb3ath notif mt3 ajout
+        String notificationMessage = deadline.getDescription() + " \n- Nouveau deadline.";
+        Notification notification = new Notification(notificationMessage, "etudiant", new Date());
+        notificationRepository.save(notification);
         return deadlineRepository.save(deadline);
     }
 
     @Override
     public void modifierDeadline(Deadline deadline) {
+        //yeb3ath notif fel mod
+        String notificationMessage = deadline.getDescription() + " \n- Deadline modifiée .";
+        Notification notification = new Notification(notificationMessage, "etudiant", new Date());
+        notificationRepository.save(notification);
         deadlineRepository.save(deadline);
     }
 
